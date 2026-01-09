18ddefe0aa9f9812be3e53e7a0f57fa424a54659
Brian <ndanumumo93@gmail.com>
2026-01-09 12:45:14 +0300

    first commit

--- src/constants/z-indexes.ts
+++ src/constants/z-indexes.ts
@@ -3,5 +3,5 @@ export const popover_zindex = Object.freeze({
     TOOLBAR: 100,
     TRANSACTION: 10,
     SUMMARY_TOOLTIPS: 5,
-    RUN_PANEL: 11,
+    RUN_PANEL: 1001, // Higher than header (z-index: 1000) to ensure visibility on mobile
 });
